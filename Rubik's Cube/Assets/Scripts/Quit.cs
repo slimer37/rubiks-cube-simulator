07a90eafce371d7ad2a99ad6594ed0e118365b79
slimer37 <59073967+slimer37@users.noreply.github.com>
2020-05-12 19:07:53 -0700

    Add ability to quit with esc

---
+++ Rubik's Cube/Assets/Scripts/Quit.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Quit : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+            #endif
+
+            Application.Quit();
+        }
+    }
+}
